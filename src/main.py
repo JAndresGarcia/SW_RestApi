fa18ad4773e0739c1e96c04f10dc9b4abddba5a1
JAndresGarcia <dressg7@gmail.com>
2022-09-17 02:42:42 +0000

    Completado

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People
 #from models import Person
 
 app = Flask(__name__)
@@ -30,16 +30,184 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets ))
+    print(planets)
+    return jsonify(planets), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/planet/<planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if isinstance(planet, Planet):
+        return jsonify(planet.serialize()), 200
+    else:
+        return jsonify({
+            "message":"planeta no encontrado"
+        })
 
-    return jsonify(response_body), 200
+@app.route('/planet', methods=['POST'])
+def register_planet():
+    planet = Planet()
+    body = request.json
+    
+    planet.climate = body["climate"]
+    planet.created = body["created"]
+    planet.diameter = body["diameter"]
+    planet.gravity = body["gravity"]
+    planet.name = body["name"]
+    planet.orbital_period = body["orbital_period"]
 
-# this only runs if `$ python src/main.py` is executed
+    db.session.add(planet)
+    try:        
+        db.session.commit()
+        return jsonify(planet.serialize()), 201
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"error"}), 400
+
+@app.route('/planet/<planet_id>', methods=['PUT'])
+def update_planet():
+    planet = Planet()
+    body = request.json
+    planet = planet.query.get(planet_id)
+
+    planet.id = planet_id
+    planet.climate = body["climate"]
+    planet.created = body["created"]
+    planet.diameter = body["diameter"]
+    planet.gravity = body["gravity"]
+    planet.name = body["name"]
+    planet.orbital_period = body["orbital_period"]
+
+    db.session.add(planet)
+
+    try:        
+        db.session.commit()
+        return jsonify(planet.serialize()), 201
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"error"}), 400
+
+@app.route('/planet/<planet_id>', methods=['PATCH'])
+def partial_update_planet():
+    planet = Planet()
+    body = request.json
+    planet = planet.query.get(planet_id)
+
+    db.session.add(planet)
+
+    try:        
+        db.session.commit()
+        return jsonify(planet.serialize()), 201
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"error"}), 400
+
+@app.route('/planet/<planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = Planet()
+    body = request.json
+    planet = planet.query.delete(planet_id)
+
+    if planet:
+        return jsonify(planet), 200
+    else:
+        return jsonify({"message: ID no existe"}), 400
+
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    people = list(map(lambda people: people.serialize(), people ))
+    print(people)
+    return jsonify(people), 200
+
+@app.route('/people/<person_id>', methods=['GET'])
+def person(person_id):
+    person = People.query.get(person_id)
+    if isinstance(person, People):
+        return jsonify(person.serialize()), 200
+    else:
+        return jsonify({
+            "message":"personaje no encontrado"
+        })
+
+@app.route('/people', methods=['POST'])
+def people():
+    people = People()
+    body = request.json
+    
+    people.name = body["name"]
+    people.height = body["height"]
+    people.mass = body["mass"]
+    people.hairColor = body["hairColor"]
+    people.skinColor = body["skinColor"]
+
+    db.session.add(people)
+    try:        
+        db.session.commit()
+        return jsonify(people.serialize()), 201
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"error"}), 400
+
+@app.route('/people/<person_id>', methods=['PUT'])
+def person():
+    person = Planet()
+    body = request.json
+    person = person.query.get(person_id)
+
+    people.name = body["name"]
+    people.height = body["height"]
+    people.mass = body["mass"]
+    people.hairColor = body["hairColor"]
+    people.skinColor = body["skinColor"]
+
+    db.session.add(person)
+
+    try:        
+        db.session.commit()
+        return jsonify(person.serialize()), 201
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"error"}), 400
+
+@app.route('/people/<person_id>', methods=['PATCH'])
+def partial_update_person():
+    person = People()
+    body = request.json
+    person = person.query.get(person_id)
+
+    db.session.add(person)
+
+    try:        
+        db.session.commit()
+        return jsonify(person.serialize()), 201
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"error"}), 400
+
+@app.route('/people/<person_id>', methods=['DELETE'])
+def delete_person(person_id):
+    person = People()
+    body = request.json
+    person = person.query.delete(person_id)
+
+    if person:
+        return jsonify(person), 200
+    else:
+        return jsonify({"message: ID no existe"}), 400
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -8,12 +8,56 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    climate = db.Column(db.String(80), unique=False, nullable=False)
+    created = db.Column(db.String(80), unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
+    gravity = db.Column(db.Integer, unique=False, nullable=False)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "created": self.created,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "orbital_period": self.orbital_period
+        }
+
+    def __repr__(self):  ##como se muestra al hacer print()
+        return '<Planet %r>' % self.name
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable = False)
+    height = db.Column(db.Integer, nullable = False)
+    mass = db.Column(db.Integer, nullable = False)
+    hairColor = db.Column(db.String(250), nullable = False)
+    skinColor = db.Column(db.String(250), nullable = False)
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hairColor": self.hairColor,
+            "skinColor": self.skinColor
+        }
+
+    def __repr__(self):
+        return '<People %r>' % self.name
